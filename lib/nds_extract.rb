b61e4fd23bb13a1a5dfd1361514dd59efd6df2cc
Kent Chan <kntchn@gmail.com>
2020-01-02 22:55:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'directors_database' 
+require 'directors_database'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
